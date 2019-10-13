5cb04639dfa6c0f96f439c84c3acec542be701cb
kim <wjdrms1919@gmail.com>
2019-10-13 15:13:36 +0900

    Fix:Code

--- src/index.js
+++ src/index.js
@@ -9,8 +9,11 @@ const serviceWorker = require('./serviceWorker');
 ReactDOM.render(
     <React.Fragment>
         <Root />
+        {process.env.NODE_ENV === 'production'
+          && <script src="https://polyfill.io/v3/polyfill.min.js?features=es6,es7,es8,es9,NodeList.prototype.forEach&flags=gated" />}
     </React.Fragment>,
      document.getElementById('root')
+     
      );
 serviceWorker.unregister();
 
